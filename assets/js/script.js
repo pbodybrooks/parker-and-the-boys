1012e1fec4d5f40aec0afe67410ad6c486a93b68
Parker <parkerlamarbrook@gmail.com>
2023-03-30 21:29:30 -0600

    Weather data is being returned. Working on distance calculation.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 // 30-day Forecast URL: https://pro.openweathermap.org/data/2.5/forecast/climate?lat={lat}&lon={lon}&appid={API key}
 // GEO URL http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
-// forecast30URL = "https://pro.openweathermap.org/data/2.5/forecast/climate?lat=" + lat + "&lon=" + lon + "&appid=" + OpenWeatherAPIKey;
+
 // "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + OpenWeatherAPIKey;
 
 // API Keys:
 const OpenWeatherAPIKey = "fe8c1985df685a70d00e6d5098ec6a2d";
 
 // Global Vars
-let lat;
-let lon;
+// let lat;
+// let lon;
 let city = "tulsa";
 
 function getCoords(city){
@@ -25,28 +25,73 @@ function getCoords(city){
             let lat = geoData.coord.lat;
             let lon = geoData.coord.lon;
             console.log(geoData);
-            console.log("Lat: " + lat + "\nLon: " + lon);
-            })
             
-            getForecast();
+
+            getForecast(lat, lon, city);
+            })    
 }
 
-function getForecast(lat, lon){
-    forecast30URL = "https://pro.openweathermap.org/data/2.5/forecast/climate?lat=" + lat + "&lon=" + lon + "&appid=" + OpenWeatherAPIKey;
+function getForecast(lat, lon, city){
+    let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + OpenWeatherAPIKey + "&units=imperial";
+    console.log(city + " Lat: " + lat + "\nLon: " + lon);
     // fetch forecast data
-    fetch(forecast30URL)
+    fetch(forecastURL)
         .then(function (response) {
         return response.json();
         })
         .then(function (forecastData) {
-        console.log('The thirty-day forecast in ' + city + "is:");
+        console.log('Five Day Forecast in ' + city + " is:");
         console.log(forecastData);
-
+        })
 }
 
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      console.log("Geolocation is not supported by this browser.")
+    }
+}
+
+// parseInt
+
+function showPosition(position, lat, lon) {
+    userLat = position.coords.latitude;
+    userLon = position.coords.longitude;
+    console.log ("User lat & lon: " + userLat + userLon)
+
+    getDistanceFromLatLonInKm(userLat, userLon, lat, lon);
+}
+  
+
 window.addEventListener('load', getCoords(city));
+window.addEventListener('load', getLocation);
 
 
+function getDistanceFromLatLonInKm(userLat, userLon, lat, lon) {
+    const R = 6371; // Radius of the earth in km
+    console.clear();
+    console.log("\n");
+    console.log("\n");
+    const dLat = deg2rad(userLat-lat);  // deg2rad below
+    const dLon = deg2rad(userLon-lon); 
+    const a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat)) * Math.cos(deg2rad(userLat)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    const d = R * c; // Distance in km
+    console.log("Distance: " + d);
+    return d;
+}
+
+function deg2rad(deg) {
+return deg * (Math.PI/180)
+}
+  
+
+  
 
 
 
